ab9a4abc74042580b0389274d05e2ebf68cb79b7
Martin Persson <jm-persson@hotmail.com>
2021-04-29 15:51:45 +0200

    Added dummy pictures to startpage

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,9 +1,41 @@
 <template>
-  <h1>Himma!</h1>
+  <div class="home-content">
+    <h1>Clear-BnB</h1>
+    <form submit.prevent="">
+      <input type="text" placeholder="Enter location..." />
+      <button class="btn" @click="searchByLocation">Search</button>
+    </form>
+    <div class="image-container">
+      <img src="../../public/images/House1.jpg" alt="" />
+      <img src="../../public/images/House2.jpg" alt="" />
+      <img src="../../public/images/House3.jpg" alt="" />
+      <img src="../../public/images/House4.jpg" alt="" />
+      <img src="../../public/images/House5.jpg" alt="" />
+      <img src="../../public/images/House6.jpg" alt="" />
+      <img src="../../public/images/House7.jpg" alt="" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    searchByLocation() {},
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.home-content {
+  max-width: 60rem;
+  margin: auto;
+}
+.image-container {
+  margin-top: 5rem;
+}
+.image-container img {
+  height: 400px;
+  width: 300px;
+  object-fit: cover;
+}
+</style>
