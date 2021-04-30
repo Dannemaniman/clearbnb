b8dc1f2133a36592c1d4381aefc411605e095dda
Martin Persson <jm-persson@hotmail.com>
2021-04-30 10:31:09 +0200

    writing componentItem

---
+++ Frontend/src/components/GalleryComponentItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div @click="goToDetails" :class="'image'" :key="'index'">
+    <!-- v-bind: (short ':') binds an attribute
+          to a reactive varaible -->
+    <img v-bind:src="property.imageUrl" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      image: 'img-' + (index + 1),
+    };
+  },
+  props: ['property'],
+
+  methods: {
+    goToDetails() {
+      this.$router.push('/property/' + this.property.id);
+    },
+  },
+};
+</script>
+
+<style></style>
