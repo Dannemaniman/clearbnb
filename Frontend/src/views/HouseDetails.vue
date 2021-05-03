1732eec0076af91f6f72fd5213b01edd847aff37
Martin-Persson <jm-persson@hotmail.com>
2021-05-03 13:19:34 +0200

    Filtered out desired home

--- Frontend/src/views/HouseDetails.vue
+++ Frontend/src/views/HouseDetails.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1>Details</h1>
+  <div class="content">
+    <h1>{{ home.title }}</h1>
+    <!-- <img class="img-1" :src="home.images[0]" />
+    <img class="img-2" :src="home.images[0]" />
+    <img class="img-3" :src="home.images[0]" />
+    <img class="img-4" :src="home.images[0]" /> -->
+    {{ home.description }}
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    home() {
+      return this.$store.state.houses.find(
+        (h) => h.id == this.$route.params.id
+      );
+    },
+  },
+};
 </script>
 
 <style></style>
