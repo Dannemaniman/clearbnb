c2959f62743ccdb0d3ac5e51c9ba9447344c1a0c
Dannemaniman <daniellangas91@gmail.com>
2021-05-06 01:04:51 +0200

    Created BookingModal, with small calender component and GuestSum component

--- Frontend/src/views/HouseDetails.vue
+++ Frontend/src/views/HouseDetails.vue
@@ -1,13 +1,16 @@
 <template>
   <h1>Details</h1>
   <Hosts />
+  <BookingModal />
 </template>
 
 <script>
 import Hosts from "../components/Hosts.vue"
+import BookingModal from "../components/BookingModal.vue"
 export default {
   components: {
-    Hosts
+    Hosts,
+    BookingModal
   }
 };
 </script>
