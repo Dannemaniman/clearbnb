7b394c7ff152d7974c2e889649123fef0f6de8c7
Martin Persson <jm-persson@hotmail.com>
2021-05-20 10:18:22 +0200

    specified minZoom

--- Frontend/src/components/MapComponent.vue
+++ Frontend/src/components/MapComponent.vue
@@ -28,6 +28,7 @@ export default {
           attribution:
             'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
           maxZoom: 18,
+          minZoom: 2,
           id: 'mapbox/streets-v11',
           tileSize: 512,
           zoomOffset: -1,
