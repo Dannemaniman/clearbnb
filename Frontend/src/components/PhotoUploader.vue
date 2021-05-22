42a4ffe62b91aa662bc24aad0bc8c2328ee2adfb
Martin-Persson <jm-persson@hotmail.com>
2021-05-22 11:39:54 +0200

    created html for drag-drop

--- Frontend/src/components/PhotoUploader.vue
+++ Frontend/src/components/PhotoUploader.vue
@@ -1,62 +1,77 @@
 <template>
-  <section>
-    <div class="image-container">
-      <img v-for="(image, index) in thumbnail" :src="image" :key="index" class="chosen-image"/>
+  <form @submit.prevent="sendFile" enctype="multipart/form-data">
+    <div
+      v-if="message"
+      :class="`message ${error ? 'is-danger' : 'is-success'} `"
+    >
+      <div class="message-body">{{ message }}</div>
     </div>
-      <input type="file" @change="chosenFile" style="display: none" ref="inputFile"/>
-      <button @click="$refs.inputFile.click()">Choose File</button>
-  </section>
+    <div class="dropzone">
+      <input type="file" class="input-field" />
+
+      <p v-if="!uploading" class="call-to-action">Drag your files here</p>
+
+      <p v-if="uploading" class="progress-bar"></p>
+    </div>
+  </form>
 </template>
 
 <script>
 export default {
-  emit:['photo'],
-  data(){
+  name: 'Dropzone',
+
+  data() {
     return {
+      file: '',
+      messege: '',
+      error: false,
+      uploading: false,
+      // gammalt under
       selectedFiles: [],
-      thumbnail: []
-    }
+      thumbnail: [],
+    };
   },
+  emit: ['photo'],
   methods: {
     chosenFile(event) {
-      this.selectedFiles.push(event.target.files[0])
-      this.thumbnail.push(URL.createObjectURL(event.target.files[0]))
-      this.$emit('photo', this.selectedFiles)
-    }
-  }
-}
+      this.selectedFiles.push(event.target.files[0]);
+      this.thumbnail.push(URL.createObjectURL(event.target.files[0]));
+      this.$emit('photo', this.selectedFiles);
+    },
+  },
+};
 </script>
 
 <style scoped>
+section {
+  margin-top: 1rem;
+}
 
-  section {
-    margin-top: 1rem;
-  }
+.image-container {
+  margin: 0 auto;
+  height: 11rem;
+  min-width: 11rem;
+  width: fit-content;
+  max-height: 20rem;
+  display: flex;
+  flex-direction: row;
+  text-align: left;
+  background-color: white;
+  border: 1px solid black;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
+    rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+  padding: 0.5rem;
+}
 
-  .image-container {
-    margin: 0 auto;
-    height: 11rem;
-    min-width: 11rem;
-    width: fit-content;
-    max-height: 20rem;
-    display: flex;
-    flex-direction: row;
-    text-align: left;
-    background-color: white;
-    border: 1px solid black;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
-    padding: 0.5rem;
-  }
+.chosen-image {
+  height: 10rem;
+  width: 10rem;
+}
 
-  .chosen-image {
-    height: 10rem;
-    width: 10rem;
-  }
-
-  button {
-    padding: 0.6rem;
-    margin: 1rem;
-    cursor: pointer;
-    border-radius: 10px;
-  }
+button {
+  padding: 0.6rem;
+  margin: 1rem;
+  cursor: pointer;
+  border-radius: 10px;
+}
 </style>
