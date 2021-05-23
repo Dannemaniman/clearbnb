1df9ee9e2e433d4bdc5a29fc81da1b4cd58ebe1b
Martin-Persson <jm-persson@hotmail.com>
2021-05-23 09:37:55 +0200

    added and removed images + edited grid in details

--- Frontend/src/views/HouseDetails.vue
+++ Frontend/src/views/HouseDetails.vue
@@ -3,7 +3,7 @@
     <h1>{{ home.title }}</h1>
     <div class="images">
       <img :src="home.images[0]" alt="" class="img-1" />
-      <img :src="home.images[0]" alt="" class="img-2" />
+      <img :src="home.images[1]" alt="" class="img-2" />
       <img :src="home.images[0]" alt="" class="img-3" />
       <img :src="home.images[0]" alt="" class="img-4" />
       <img :src="home.images[0]" alt="" class="img-5" />
@@ -88,18 +88,59 @@ export default {
   display: grid;
   grid-template-rows: 1fr 1fr 1fr 1fr;
   grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-areas:
+    'img-1 img-1 img-1 img-2'
+    'img-1 img-1 img-1 img-3'
+    'img-1 img-1 img-1 img-4'
+    'img-5 img-6 img-7 img-8';
   padding: 1rem;
   gap: 0.3rem;
 }
 .images img {
   width: 100%;
+  max-height: 100%;
+  object-fit: cover;
 }
-.images img:nth-child(1) {
+.images .img-1 {
+  grid-area: img-1;
+  object-fit: cover;
+  max-height: 30.6rem;
+}
+.img-2 {
+  grid-area: img-2;
+  height: 10rem;
+}
+.img-3 {
+  grid-area: img-3;
+  height: 10rem;
+}
+.img-4 {
+  grid-area: img-4;
+  height: 10rem;
+}
+.img-5 {
+  grid-area: img-5;
+  height: 10rem;
+}
+.img-6 {
+  grid-area: img-6;
+  height: 10rem;
+}
+.img-7 {
+  grid-area: img-7;
+  height: 10rem;
+}
+.img-8 {
+  grid-area: img-8;
+  height: 10rem;
+}
+
+/* .images img:nth-child(1) {
   grid-row-start: 1;
   grid-row-end: 4;
   grid-column-start: 1;
   grid-column-end: 4;
-}
+} */
 
 .reviews {
   display: flex;
