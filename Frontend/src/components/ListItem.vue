bb5bc7fe7c1031c957749d2abe70674a72b386b2
LarssonLarsJim <77106246+LarssonLarsJim@users.noreply.github.com>
2021-05-25 22:39:08 +0200

    Started styling confirmpage and  listitem

--- Frontend/src/components/ListItem.vue
+++ Frontend/src/components/ListItem.vue
@@ -4,12 +4,16 @@
     <div class="item-info">
       <p class="item-type">{{ title }}</p>
       <p class="item-adress">{{ address }}, {{ city }}</p>
-      <p class="item-date">{{ chosenDate.start }} - {{ chosenDate.end }}</p>
+      <p class="item-date">
+        Date: {{ chosenDate.start }} - {{ chosenDate.end }}
+      </p>
     </div>
     <button v-if="reviewable" class="review-button" @click="popReviewModal">
       Review
     </button>
-    <button class="cancel-button" @click="deleteBooking(houseId)">Cancel</button>
+    <button class="cancel-button" @click="deleteBooking(houseId)">
+      Cancel
+    </button>
   </article>
   <ReviewModal
     :houseId="houseId"
@@ -59,20 +63,20 @@ article {
   margin: 0 auto;
   position: relative;
   border: 1px solid transparent;
-  background-color: whitesmoke;
-  border-bottom: 2px solid black;
+  border-radius: 8px;
   transition: all 0.2s ease;
 }
 
 article:hover,
 article:active {
   box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
-  border-bottom: 2px solid black;
+  border-bottom: 1px solid black;
 }
 
 img {
   height: 150px;
   width: 150px;
+  border-radius: 8px 0px 0px 8px;
 }
 
 .cancel-button {
@@ -84,8 +88,23 @@ img {
   border-top: none;
   border-right: none;
   outline: none;
-  border-radius: 0 0 0 10px;
+  border-radius: 0 8px 0 10px;
   cursor: pointer;
+  background-image: linear-gradient(
+    147deg,
+    rgb(254, 149, 0) 0%,
+    rgb(254, 120, 0) 84%
+  );
+  border: #4b4b4b;
+}
+
+.cancel-button:hover {
+  background-image: linear-gradient(
+    147deg,
+    rgb(185, 108, 0) 0%,
+    rgb(228, 108, 3) 84%
+  );
+  color: #4b4b4b;
 }
 
 .review-button {
@@ -109,21 +128,19 @@ button:active {
 }
 
 .item-type {
-  font-size: 2rem;
-  margin-top: 4rem;
+  font-size: 1.5rem;
+  margin-top: 1rem;
   margin-left: 1rem;
   text-align: left;
 }
 
 .item-adress {
-  margin-left: 1rem;
+  margin-left: 1.6rem;
 }
 
 .item-date {
-  position: absolute;
-  top: 0;
-  right: 5rem;
-  margin-right: 1rem;
-  font-size: 0.7rem;
+  text-align: left;
+  margin-left: 1.6rem;
+  font-size: 1rem;
 }
 </style>
