11f068b19d71ee397c57ad01320ba9c8d074c848
LarssonLarsJim <77106246+LarssonLarsJim@users.noreply.github.com>
2021-05-26 22:43:53 +0200

    New files

--- Frontend/src/components/SearchResultItem.vue
+++ Frontend/src/components/SearchResultItem.vue
@@ -1,7 +1,15 @@
 <template>
-  <article @click="goToDetails">
-    <img :src="house.images[0]" />
-    <h2>{{ house.propertyType }}</h2>
+  <article class="house" @click="goToDetails">
+    <div class="image-div">
+      <img :src="house.images[0]" />
+      <div class="overlay">
+        <div class="text">
+          {{ house.propertyType }} with {{ house.accommodation.beds }} beds
+          <br />
+          {{ house.price }} SEK / Night
+        </div>
+      </div>
+    </div>
     <h2>{{ house.city }}</h2>
     <h3>{{ house.address }}</h3>
   </article>
@@ -21,12 +29,58 @@ export default {
 
 <style scoped>
 .house {
+  display: flex;
+  flex-direction: column;
+  align-self: center;
   width: 45%;
+  min-width: 20rem;
+  background-image: url('https://www.getaccept.com/hubfs/Product%20pages%202.0/Product%20tour/bottom%20wave.svg%27');
+  margin-top: 3rem;
+  box-shadow: 1px 3px 5px rgb(0 0 0 / 80%);
+  transition: 0.2s ease-out;
+}
+
+.image-div {
+  position: relative;
+}
+.house .overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: 0.5s ease;
+  background-color: #000;
+  line-height: 2rem;
+  border-radius: 0 0 8rem 0rem;
+}
+.overlay:hover {
+  opacity: 0.7;
+}
+.overlay .text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+  width: 100%;
 }
 img {
   width: 100%;
-  height: 25rem;
-  border-radius: 2rem;
+  height: 15rem;
+  border-radius: 0 0 8rem 0rem;
   cursor: pointer;
+  object-fit: cover;
+}
+h2,
+h3 {
+  margin: 0.5rem;
 }
 </style>
