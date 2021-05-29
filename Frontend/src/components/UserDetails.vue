e5d85cfa47df4f7dcbe8ba4e9c7fd3bddc9b5969
Martin-Persson <jm-persson@hotmail.com>
2021-05-29 22:44:33 +0200

    Flex boxed userpage

--- Frontend/src/components/UserDetails.vue
+++ Frontend/src/components/UserDetails.vue
@@ -1,39 +1,37 @@
 <template>
-<section>
-<h1>User Credentials</h1>
-  <div class="user-details" v-if="user">
-    <div class="image">
-      <img :src="user.image" alt="" />
+  <section>
+    <h1>User Credentials</h1>
+    <div class="user-details" v-if="user">
+      <div class="image">
+        <img :src="user.image" alt="" />
+      </div>
+      <div class="info">
+        <h2>{{ user.fullName }}</h2>
+        <div class="email">
+          <h4>Email:</h4>
+          <p>{{ user.email }}</p>
+        </div>
+        <div class="name">
+          <h4>Name:</h4>
+          <p>{{ user.fullName }}</p>
+        </div>
+        <div class="gender">
+          <h4>Gender:</h4>
+          <p>{{ user.gender }}</p>
+        </div>
+        <div class="description">
+          <h4>Description:</h4>
+          <p>{{ user.description }}</p>
+        </div>
+      </div>
     </div>
-    <div class="info">
-      <h2>{{ user.fullName }}</h2>
-      <table>
-        <tr>
-          <th>Email:</th>
-          <td> {{ user.email }}</td>
-        </tr>
-        <tr>
-          <th>Name:</th>
-          <td> {{ user.fullName }}</td>
-        </tr>
-        <tr>
-          <th>Gender:</th>
-          <td> {{ user.gender }}</td>
-        </tr>
-        <tr>
-          <th colspan="3">Description: </th>
-        </tr>                  
-        <tr>
-          <td colspan="3">{{user.description}}</td>
-        </tr>
-      </table>
+    <button class="create-button" @click="showCreate = !showCreate">
+      Edit User Info
+    </button>
+    <div v-if="showCreate">
+      <UserCreation :user="user" />
     </div>
-  </div>
-  <button class="create-button" @click="showCreate = !showCreate">Edit User Info</button>
-  <div v-if="showCreate">
-    <UserCreation :user="user" />
-  </div>
-</section>    
+  </section>
 </template>
 
 <script>
@@ -60,7 +58,6 @@ export default {
 </script>
 
 <style scoped>
-
 section {
   padding: 2rem;
 }
@@ -76,7 +73,8 @@ th {
   text-align: center;
 }
 
-td, th {
+td,
+th {
   /* border-bottom: 1px solid rgba(0, 0, 0, 0.164); */
   /* border: 1px solid orange; */
   padding: 1rem 3rem;
@@ -84,7 +82,6 @@ td, th {
   /* border: 1px solid black; */
   line-height: 120%;
   border-bottom: 1px solid rgba(0, 0, 0, 0.226);
-
 }
 
 /* .create-button {
@@ -114,54 +111,93 @@ button {
   background-repeat: no-repeat;
   background-size: contain;
   background-size: cover;
-  filter:brightness(130%)
+  filter: brightness(130%);
 }
 
-button:hover, button:active  {
+button:hover,
+button:active {
   box-shadow: rgba(0, 0, 0, 0.25) 0px 5px 15px;
-  filter:brightness(100%)
+  filter: brightness(100%);
 }
 
 h1 {
   margin: 4rem auto;
-  font-size: 3rem;
+  font-size: 2rem;
 }
 
 h2 {
-  margin: 3rem auto;
-  font-size: 2rem;
+  margin: 1rem 0;
+  font-size: 1.5rem;
 }
 
 .user-details {
   display: flex;
   flex-direction: row;
-     background-color: rgb(235, 235, 235);
-    padding: 2rem;
-    box-shadow: rgba(38, 57, 77, 0.3) 0px 20px 30px -10px;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid rgba(0, 0, 0, 0.082);
-    border-bottom: 2px solid rgba(0, 0, 0, 0.335);
+  flex-wrap: wrap;
+  background-color: rgb(235, 235, 235);
+  padding: 2rem;
+  box-shadow: rgba(38, 57, 77, 0.3) 0px 20px 30px -10px;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid rgba(0, 0, 0, 0.082);
+  border-bottom: 2px solid rgba(0, 0, 0, 0.335);
+  line-height: 145%;
 }
 
 .image {
-  width: 50%;
+  width: 48%;
+  margin: 0.5rem;
 }
 
 img {
-  margin-top: 4rem;
-  width: 300px;
-  height: 300px;
+  margin-top: 2rem;
+  max-width: 100%;
+  height: auto;
   border-radius: 50%;
-  object-fit: cover;
+  object-fit: contain;
 }
 
 .info {
-  width: 50%;
+  width: 48%;
+  display: flex;
+  flex-flow: wrap;
+  text-align: left;
 }
 
-.info h1 {
-  margin-top: 4rem;
-  padding-bottom: 0.5rem;
+.info h2 {
+  margin-top: 2rem;
+  max-width: 100%;
+}
+.info p {
+  display: inline;
+}
+.info h4 {
+  display: inline;
+  margin-bottom: 0.1rem;
+  margin-right: 0.2rem;
+}
+.email,
+.name,
+.gender {
+  width: 100%;
+  margin: 0.1rem;
+}
+.info .description {
+  display: flex;
+  flex-direction: column;
+}
+.info .description h4 {
+  width: 100%;
+  margin: 0;
+}
+.description p {
+  display: contents;
+  margin-top: 0.2rem;
+}
+@media screen and (max-width: 600px) {
+  .image,
+  .info {
+    width: 100%;
+  }
 }
 </style>
